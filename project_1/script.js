7aeb21ebefa393f4948632a32508adf08c26e38f
Brandon Naude <naudebrandon@gmail.com>
2023-08-12 16:55:16 +0200

    set the frame rate

--- project_1/script.js
+++ project_1/script.js
@@ -10,6 +10,9 @@ const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
+
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
@@ -17,6 +20,11 @@ function animate(){
     // s--> Source | d--> Destination
     // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
     ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrames == 0){
+        if (frameX < 6) frameX++;
+        else frameX = 0;
+    }
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 
